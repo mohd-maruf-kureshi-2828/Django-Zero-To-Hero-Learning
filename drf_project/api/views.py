ca8140f11550e3daa3aff33e059f9768c61959ae
mohd-maruf-kureshi-2828 <mohamedmarufkureshikureshi@gmail.com>
2026-01-05 19:34:07 +0530

    Learning drf core

---
+++ drf_project/api/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Student
+from .serializers import StudentSerializer
+# Create your views here.
+
+@api_view(['GET','POST'])
+def student_api(request):
+
+    if request.method=='GET':
+        student=Student.objects.all()
+        serializer=StudentSerializer(student,many=True)
+        return Response(serializer.data)
+    
+    if request.method=="POST":
+        serializer=StudentSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+
+
